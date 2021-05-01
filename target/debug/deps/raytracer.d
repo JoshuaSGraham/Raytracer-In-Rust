03b19f9ff7ececc86db4d2ec264e758fec92de92
joshua graham <joshscottgraham@gmail.com>
2021-05-01 15:12:21 +0100

    Added inital starting files for the project

---
+++ target/debug/deps/raytracer.d
@@ -0,0 +1,5 @@
+C:\Users\joshs\Documents\Work\LearnRust\Raytracer-In-Rust\target\debug\deps\raytracer.exe: src\main.rs
+
+C:\Users\joshs\Documents\Work\LearnRust\Raytracer-In-Rust\target\debug\deps\raytracer.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/raytracer.d
@@ -0,0 +1,1 @@
+C:\Users\joshs\Documents\Work\LearnRust\Raytracer-In-Rust\target\debug\raytracer.exe: C:\Users\joshs\Documents\Work\LearnRust\Raytracer-In-Rust\src\main.rs
