f48f6299170f9eb3ba338f2ac11aa25c76781a43
joshua graham <joshscottgraham@gmail.com>
2021-05-01 15:24:58 +0100

    added inital main.rs code to test writing to a file of type .ppm
    image.ppm is created with cargo run > image.ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
 fn main() {
-    println!("Hello, world!");
+    //image 
+    let image_width = 256;
+    let image_height = 256;
+
+    //render
+    print!("P3\n{} {} \n255\n", image_width, image_height);
+
+    for height_index in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {}", height_index);
+        for width_index in 0..image_width {
+            
+            let red = width_index as f64 / (image_width -1) as f64;
+            let green = height_index as f64 / (image_height -1) as f64;
+            let blue = 0.25; 
+
+            let ir : i32 = (255.999 * red) as i32;
+            let ig : i32 = (255.999 * green) as i32;
+            let ib : i32 = (255.999 * blue) as i32;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
+    eprintln!("\nDone");
 }
