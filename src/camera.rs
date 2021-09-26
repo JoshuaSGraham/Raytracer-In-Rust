d4ea6424cc746d758476be805f7ee01a756e9402
joshua graham <joshscottgraham@gmail.com>
2021-09-26 22:23:45 +0100

    Started moveable camera

--- src/camera.rs
+++ src/camera.rs
@@ -12,13 +12,28 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn camera() -> Camera {
+    pub fn camera(look_from: Vec3, look_at: Vec3, vup: Vec3, vfov: f64, aspect: f64) -> Camera {
+
+        let mut u = Vec3::default();
+        let mut v = Vec3::default();
+        let mut w = Vec3::default();
+
+
+        let theta = vfov * std::f64::consts::PI / 180.0;
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
+
+        let origin = look_from;
+        w = Vec3::unit_vector(&(look_from - look_at));
+        u = Vec3::unit_vector(&Vec3::cross(&vup, &w));
+        v = Vec3::cross(&w, &u);
+
         Camera {
 
-            origin: Vec3::new(0.0, 0.0, 0.0),
-            lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
-            horizontal: Vec3::new(4.0, 0.0, 0.0),
-            vertical: Vec3::new(0.0, 2.0, 0.0),
+            origin,
+            lower_left_corner: origin - half_width * u - half_height * v - w,
+            horizontal: 2.0 * half_height * u,
+            vertical: 2.0 * half_height * v,
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn main() {
     //image
 
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 600;
+    let image_width = 450;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
     let max_value = 255;
     let sample_count = 100;
@@ -74,6 +74,7 @@ fn main() {
     )));
     
     let world = HittableList::new(list);
+
     let cam = Camera::camera();
     let mut rng = rand::thread_rng();
 
